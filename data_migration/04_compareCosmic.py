33163fff87b3ec8278d9362872286020d93b1bab
sishirsubedi <subedisishir@gmail.com>
2018-10-09 14:29:39 -0500

    updated cosmic comparisons

--- data_migration/04_compareCosmic.py
+++ data_migration/04_compareCosmic.py
@@ -4,6 +4,15 @@ import glob
 import optparse
 import pandas as pd
 
+def findUniq(ids):
+    uniq=[]
+    for item in ids:
+        items = item.replace(" ", "").split(',')
+        for x in items:
+            uniq.append(x)
+    return uniq
+
+
 def compare(instrument, assay, runID,variantcaller,coveragecaller, sample):
 
     environment = 'ngs_validation'
@@ -25,22 +34,30 @@ def compare(instrument, assay, runID,variantcaller,coveragecaller, sample):
 
     df_file_v2_coverage = pd.read_csv(file_v2_coverage[0], header=None, sep='\t')
     df_file_v2_coverage = df_file_v2_coverage.iloc[:,[2,3,4,5]]
+    total_variants = df_file_v2_coverage.shape[0]
     df_file_v2_coverage.columns=['chr','pos', 'ref','alt']
 
-    df_cosmic82 = pd.read_csv('/home/hhadmin/hmvv_validation/cosmic_82.txt', sep='\t')
+    ## run this to groupby chr pos ref alt first:
+    # df_cosmic82_combine = df_cosmic82.groupby(['chr','pos', 'ref','alt'])['cosmicID'].apply(', '.join).reset_index()
+    # df_cosmic86_combine = df_cosmic86.groupby(['chr','pos', 'ref','alt'])['cosmicID'].apply(', '.join).reset_index()
 
-    df_cosmic86 = pd.read_csv('/home/hhadmin/hmvv_validation/cosmic_86.txt', sep='\t')
+    df_cosmic82 = pd.read_csv('/home/hhadmin/hmvv_validation/cosmic_82_grouped.txt')
+    df_cosmic86 = pd.read_csv('/home/hhadmin/hmvv_validation/cosmic_86_grouped.txt')
 
 
     df_join1 = pd.merge(df_file_v2_coverage,df_cosmic82,on=['chr','pos', 'ref','alt'], how='left')
     df_join2 = pd.merge(df_join1,df_cosmic86,on=['chr','pos', 'ref','alt'], how='left')
     df_join2.columns=['chr','pos', 'ref','alt','v82','v86']
-    df_join2.dropna(inplace=True)
+    df_join2.dropna(axis=0, subset=['v82','v86'],inplace=True)
 
-    v82_uniq=df_join2['v82'].unique()
+    v82_uniq= df_join2['v82'].unique()
+    v82_uniq_2 =findUniq(v82_uniq)
+
     v86_uniq=df_join2['v86'].unique()
-    result.append([instrument,assay,runID,sample,df_join2.shape[0], len(v82_uniq),len(v86_uniq),[x for x in v82_uniq if x not in v86_uniq ],[x for x in v86_uniq if x not in v82_uniq ] ])
+    v86_uniq_2 = findUniq(v86_uniq)
 
+    print('total variants is ', total_variants)
+    result.append([instrument,assay,runID,sample,total_variants, len(v82_uniq_2),len(v86_uniq_2),[x for x in v82_uniq_2 if x not in v86_uniq_2 ],[x for x in v86_uniq_2 if x not in v82_uniq_2 ] ])
 
     df_result = pd.DataFrame(result)
     df_result.to_csv('04_cosmicResults.txt',sep='\t',index=False,header=False, mode = 'a')
