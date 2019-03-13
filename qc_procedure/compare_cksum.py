a8cf14855d37cdfcab3151bb60aded98b7f89e4b
sishirsubedi <subedisishir@gmail.com>
2019-03-13 15:54:57 -0500

    added data integrity qc

---
+++ qc_procedure/compare_cksum.py
@@ -0,0 +1,22 @@
+import sys
+import pandas as pd
+import numpy as np
+
+
+##################################
+### input variables
+##################################
+
+FILE1=sys.argv[1] #hh
+FILE2=sys.argv[2] #proton
+OUTFILE=sys.argv[3] #outfile
+
+df_1= pd.read_csv(FILE1,header=None, delim_whitespace=True)
+df_1.columns=['checksum','bytes','fname_serv1']
+
+df_2= pd.read_csv(FILE2,header=None, delim_whitespace=True)
+df_2.columns=['checksum','bytes','fname_serv2']
+
+df_join=pd.merge(df_1,df_2,on=['checksum','bytes'],how='outer',indicator=True)
+df_join.columns=['Checksum','Size','Filename_Server_1','Filename_Server_2','Match']
+df_join.to_csv(OUTFILE,sep='\t',index=False)
---
+++ qc_procedure/proton_to_hh_di_check.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+
+log()
+{
+ MESSAGE=$1
+ TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+ SCRIPT=$( basename $0 )
+ echo " [ $TIMESTAMP ] [ $SCRIPT ] $MESSAGE "
+}
+
+create_file()
+{
+  touch  $1
+  chmod 775  $1
+}
+
+find_test_files()
+{
+  find $HOME_HH -maxdepth 2  -mtime -30 -type f -name "*.bam" | shuf | head  > $TEST_FILES
+}
+
+cksum_on_hh()
+{
+  while read -r line ; do
+    cksum $line >> $HH_CKSUM
+  done < $TEST_FILES
+}
+
+cksum_on_proton()
+{
+  while read -r line ; do
+    #TO-DO try to find a way to use $HOME_HH instead of hard coding
+    mod_file_path=$(echo $line | sed "s/\/home\/proton//g")
+    echo "${HOME_PROTON}${mod_file_path}" | xargs ssh hhadmin@10.40.16.116 "cksum" >> $PROTON_CKSUM
+  done < $TEST_FILES
+}
+
+generate_report()
+{
+
+  /opt/python3/bin/python3 ${HOME_PYTHON}compare_cksum.py $HH_CKSUM $PROTON_CKSUM  $REPORT_FILE
+}
+
+main()
+{
+
+  HOME_PROTON="/results/analysis/output/Home"
+  HOME_HH="/home/proton"
+  HOME_RUN="/home/environments/qc_procedure/data_integrity/proton_to_hh/"
+  HOME_PYTHON="/home/pipelines/qc_procedure/"
+
+  CURRENTDT=`date '+%Y_%m_%d_%H_%M_%S'`
+  TEST_FILES="${HOME_RUN}${CURRENTDT}_DataIntegrity_CheckFiles.txt"
+  HH_CKSUM="${HOME_RUN}${CURRENTDT}_DataIntegrity_hh.cksum"
+  PROTON_CKSUM="${HOME_RUN}${CURRENTDT}_DataIntegrity_proton.cksum"
+  REPORT_FILE="${HOME_RUN}${CURRENTDT}_DataIntegrity_hh_proton.cksum.report.txt"
+
+  create_file  $TEST_FILES
+  create_file  $HH_CKSUM
+  create_file  $PROTON_CKSUM
+  create_file  $REPORT_FILE
+
+  log "Selecting files from Holly Hall:$HOME_HH"
+
+  find_test_files
+
+  log "cksum on Holly Hall:$HOME_HH"
+
+  cksum_on_hh
+
+  log "cksum on Proton server:$HOME_PROTON"
+
+  cksum_on_proton
+
+  log "Generating Report:$HOME_RUN"
+
+  generate_report
+
+  log "Completed"
+
+}
+
+
+main $*
