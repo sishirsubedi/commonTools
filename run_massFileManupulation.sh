709774b42da375ae9de90fbf46c665e337bc11b2
sishirsubedi <subedisishir@gmail.com>
2019-07-18 11:48:18 -0500

    altering a part of pipeline-example

--- run_massFileManupulation.sh
+++ run_massFileManupulation.sh
@@ -6,8 +6,8 @@ while read run sample
 do
   sampleName="$(echo -e "${sample}" | tr -d '[:space:]')"
   # echo "--$run--$sampleName--"
-  qsub -F "$run  $sampleName" /home/pipelines/ngs_test/shell/tmb_var_Interface.sh
-  # bash /home/pipelines/ngs_test/shell/tmb_var_Interface.sh  $run  $sampleName
+  qsub -F "$run  $sampleName" /home/pipelines/ngs_test/shell/FileManupulation.sh
+  # bash /home/pipelines/ngs_test/shell/FileManupulation.sh  $run  $sampleName
 
 done < "$input"
 IFS=$OLDIFS
